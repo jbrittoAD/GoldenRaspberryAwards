e66a4d34f7ea4dccb4c13246e3a125a4fef0d96a
Joao Carlos Britto Filho <jbritto@universo.univates.br>
2024-07-18 14:44:04 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ class TestAPI(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client()
 
-    def test_max_interval(self):
+    def test_interval(self):
         response = self.client.get('/producers')
         self.assertEqual(response.status_code, 200)
         # Adicione mais asserções conforme necessário
